267570d084d0724a1091abed4fa3492c980433ae
DahyeJang <everything888@naver.com>
2022-11-26 23:34:32 +0900

    221126

---
+++ programmers/시저 암호.js
@@ -0,0 +1,9 @@
+function solution(s, n) {
+  const alpha =
+    "abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZABCDEFGHIJKLMNOPQRSTUVWXYZ                                                  ";
+  return s
+    .split("")
+    .map((e) => alpha[alpha.indexOf(e) + n])
+    .join("");
+}
+console.log(solution("a B z", 4));
